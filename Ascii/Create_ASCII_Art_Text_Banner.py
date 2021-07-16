a1e1bd0cd9685176c835e079b1bb66ede7f71a31
AlteredAdmin <87214978+AlteredAdmin@users.noreply.github.com>
2021-07-16 08:23:23 -0400

    Recreated
    Renew!

---
+++ Ascii/Create_ASCII_Art_Text_Banner.py
@@ -0,0 +1,43 @@
+# Title:
+# Description:
+# More info:
+# =====================================================
+# Name: Altered Admin
+# Website: https://alteredadmin.github.io/
+# If you found this helpful Please consider:
+# Buymeacoffee: http://buymeacoffee.com/alteredadmin
+# BTC: bc1qhkw7kv9dtdk8xwvetreya2evjqtxn06cghyxt7
+# LTC: ltc1q2mrh9s8sgmh8h5jtra3gqxuhvy04vuagpm3dk9
+# ETH: 0xef053b0d936746Df00C9CCe0454b7b8afb1497ac
+
+from pyfiglet import Figlet
+
+text = "Altered Admin"
+
+print()
+custom_fig = Figlet(font='standard')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='Doom')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='banner3-d')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='banner3')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='cyberlarge')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='Morse')
+print(custom_fig.renderText(text))
+
+print()
+custom_fig = Figlet(font='Small')
+print(custom_fig.renderText(text))
